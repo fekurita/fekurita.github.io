6135bf54f4cb27294b37c2dbb0330f7eb74b6663
fekurita <felipekurita@gmail.com>
2023-05-26 09:25:51 -0300

    deixar assim

--- js/javaava.js
+++ js/javaava.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     nomesprofs = document.querySelectorAll('.nomeprof')
     for(nome of nomesprofs){
-    nome.innerHTML=localStorage.getItem('nome')+localStorage.getItem("prof")}
+    nome.innerHTML=localStorage.getItem('nome')}
 })
+//localStorage.getItem("prof")
