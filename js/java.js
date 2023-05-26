3725145383e26b6bcf846419f039dab4dfa1e807
fekurita <felipekurita@gmail.com>
2023-05-26 09:21:00 -0300

    teste

--- js/java.js
+++ js/java.js
@@ -2,4 +2,5 @@ document.addEventListener('DOMContentLoaded', function() {
     t = document.querySelector(".items1")
     h = t.querySelectorAll('li')
     localStorage.setItem('nome',h[0].innerHTML)
+    localStorage.setItem('prof',h[1].innerHTML)
 })
--- js/javaava.js
+++ js/javaava.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     nomesprofs = document.querySelectorAll('.nomeprof')
     for(nome of nomesprofs){
-    nome.innerHTML=localStorage.getItem('nome')}
+    nome.innerHTML=localStorage.getItem('nome')+localStorage.getItem("prof")}
 })
